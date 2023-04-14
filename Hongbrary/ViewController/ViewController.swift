2355dab73b7095ab6ba19c3f0f5dd8bd8c913f44
jeohong <jeohong59@gmail.com>
2023-04-14 17:03:43 +0900

    [FEAT] Tabbar 구현

---
+++ Hongbrary/ViewController/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  Hongbrary
+//
+//  Created by 홍정민 on 2023/04/14.
+//
+
+import UIKit
+
+class MainTabbarController: UITabBarController {
+    private lazy var allBooksViewController: UINavigationController = {
+        let allBooksViewController = UIViewController()
+        
+        let tabBarItem = UITabBarItem(title: "책", image: UIImage(systemName: "books.vertical.fill"), tag: 0)
+        allBooksViewController.tabBarItem = tabBarItem
+        
+        let navigationViewController = UINavigationController(rootViewController: allBooksViewController)
+        
+        return navigationViewController
+    }()
+    
+    private lazy var readBooksViewController: UINavigationController = {
+        let readBooksViewController = UIViewController()
+        
+        let tabBarItem = UITabBarItem(title: "내책", image: UIImage(systemName: "book.fill"), tag: 1)
+        readBooksViewController.tabBarItem = tabBarItem
+        
+        let navigationViewController = UINavigationController(rootViewController: readBooksViewController)
+        
+        return navigationViewController
+    }()
+    
+    private lazy var settingViewController: UINavigationController = {
+        let settingViewController = UIViewController()
+        
+        let tabBarItem = UITabBarItem(title: "설정", image: UIImage(systemName: "gear"), tag: 2)
+        settingViewController.tabBarItem = tabBarItem
+        
+        let navigationViewController = UINavigationController(rootViewController: settingViewController)
+        
+        return navigationViewController
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewControllers = [allBooksViewController, readBooksViewController, settingViewController]
+        configureTabBar()
+    }
+    
+    func configureTabBar() {
+        self.tabBar.tintColor = .label
+        self.tabBar.layer.cornerRadius = 20
+        self.tabBar.layer.masksToBounds = true
+        self.tabBar.layer.backgroundColor = UIColor.systemBackground.cgColor
+        self.tabBar.layer.borderColor = UIColor.lightGray.cgColor
+        self.tabBar.layer.borderWidth = 0.4
+    }
+}
