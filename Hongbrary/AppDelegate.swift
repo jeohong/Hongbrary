1a66e6a73c0f6a121936d5ee396bcd13b64f1075
jeohong <jeohong59@gmail.com>
2023-04-17 11:12:12 +0900

    [FEAT] Firebase 진입 설정

--- Hongbrary/AppDelegate.swift
+++ Hongbrary/AppDelegate.swift
@@ -6,14 +6,16 @@
 //
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Firebase 설정
+        FirebaseApp.configure()
         return true
     }
 
