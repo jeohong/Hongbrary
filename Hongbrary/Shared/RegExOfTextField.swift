2c2bdd4b6c010d76217d9586c4ca4103787cf208
jeohong <jeohong59@gmail.com>
2023-04-17 13:24:53 +0900

    [FEAT] TextField의 이메일 형식과 비밀번호 형식을 검사하는 클래스 생성

---
+++ Hongbrary/Shared/RegExOfTextField.swift
@@ -0,0 +1,22 @@
+//
+//  RegExOfTextField.swift
+//  Hongbrary
+//
+//  Created by 홍정민 on 2023/04/17.
+//
+
+import Foundation
+
+class RegExOfTextField {
+    /// Email 유효성 검사
+    func checkEmail(email: String) -> Bool {
+        let check = "([0-9a-zA-Z_-]+)@([0-9a-zA-Z_-]+)(\\.[0-9a-zA-Z_-]+){1,2}"
+        return NSPredicate(format: "SELF MATCHES %@", check).evaluate(with: email)
+    }
+    
+    /// Password 유효성 검사
+    func checkPassword(password: String) -> Bool {
+        let check = "^(?=.*[a-zA-Z])(?=.*[^a-zA-Z0-9])(?=.*[0-9]).{6,20}$"
+        return NSPredicate(format: "SELF MATCHES %@", check).evaluate(with: password)
+    }
+}
