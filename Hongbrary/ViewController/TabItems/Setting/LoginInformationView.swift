bb5173c58bb7afc8e2de04412527c91e8c60df0a
jeohong <jeohong59@gmail.com>
2023-04-17 10:24:47 +0900

    [FEAT] 설정 뷰 UI 구성

---
+++ Hongbrary/ViewController/TabItems/Setting/LoginInformationView.swift
@@ -0,0 +1,72 @@
+//
+//  LoginInformationView.swift
+//  Hongbrary
+//
+//  Created by 홍정민 on 2023/04/17.
+//
+
+import UIKit
+
+class LoginInformationView: UIView {
+    private let loginInformationLabel: UILabel = {
+        let loginInformationLabel = UILabel()
+        loginInformationLabel.text = "로그인 정보"
+        loginInformationLabel.font = UIFont.boldSystemFont(ofSize: 16)
+        loginInformationLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        return loginInformationLabel
+    }()
+    
+    let eMailLabel: UILabel = {
+        // TODO: Firebase 연동하여 현재 로그인한 사용자의 이메일 정보 표시
+        let eMailLabel = UILabel()
+        eMailLabel.font = UIFont.systemFont(ofSize: 13)
+        eMailLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        return eMailLabel
+    }()
+    
+    lazy var logOutButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .black
+        button.setTitle("로그아웃", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 15
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupLayout() {
+        addSubview(loginInformationLabel)
+        NSLayoutConstraint.activate([
+            loginInformationLabel.topAnchor.constraint(equalTo: topAnchor, constant: 20),
+            loginInformationLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20)
+        ])
+        
+        addSubview(eMailLabel)
+        NSLayoutConstraint.activate([
+            eMailLabel.topAnchor.constraint(equalTo: loginInformationLabel.bottomAnchor, constant: 4),
+            eMailLabel.leadingAnchor.constraint(equalTo: loginInformationLabel.leadingAnchor)
+        ])
+        
+        addSubview(logOutButton)
+        NSLayoutConstraint.activate([
+            logOutButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -20),
+            logOutButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            logOutButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            logOutButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+}
--- Hongbrary/ViewController/TabItems/Setting/SettingViewController.swift
+++ Hongbrary/ViewController/TabItems/Setting/SettingViewController.swift
@@ -8,26 +8,66 @@
 import UIKit
 
 class SettingViewController: UIViewController {
-    private let sampleLabel: UILabel = {
-       let label = UILabel()
-        label.text = "설정"
-        label.textColor = .red
-        label.translatesAutoresizingMaskIntoConstraints = false
+    private let loginInformationView: LoginInformationView = {
+        let logInInformationView = LoginInformationView()
+        logInInformationView.translatesAutoresizingMaskIntoConstraints = false
+        logInInformationView.layer.cornerRadius = 15
+        logInInformationView.backgroundColor = .white
+        logInInformationView.layer.borderWidth = 1
+        logInInformationView.layer.borderColor = UIColor.lightGray.cgColor
         
-        return label
+        return logInInformationView
     }()
-
+    
+    private lazy var restoreButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("유료구입 항목 복원", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitleColor(.black, for: .normal)
+        button.layer.cornerRadius = 15
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupLayout()
+        configureButtonsAction()
     }
     
     func setupLayout() {
-        self.view.addSubview(sampleLabel)
+        self.view.backgroundColor = .white
+        self.title = "설정"
+        
+        self.view.addSubview(loginInformationView)
         NSLayoutConstraint.activate([
-            sampleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            sampleLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor)
+            loginInformationView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            loginInformationView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10),
+            loginInformationView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10),
+            loginInformationView.heightAnchor.constraint(equalToConstant: 195)
         ])
+        
+        self.view.addSubview(restoreButton)
+        NSLayoutConstraint.activate([
+            restoreButton.topAnchor.constraint(equalTo: self.loginInformationView.bottomAnchor, constant: 10),
+            restoreButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10),
+            restoreButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10),
+            restoreButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    func configureButtonsAction() {
+        self.loginInformationView.logOutButton.addAction(UIAction(handler: { _ in
+            // TODO: Firebase 연동하여 로그아웃 로직 작성 후 로그인 창 뜨도록 연결
+            print("로그아웃 버튼 클릭")
+        }), for: .touchUpInside)
+        
+        self.restoreButton.addAction(UIAction(handler: { _ in
+            // TODO: 유료 구입 항목 복원 로직
+            print("유료 항목 복원 버튼 클릭")
+        }), for: .touchUpInside)
     }
 }
